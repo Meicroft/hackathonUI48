44703f0dee61adb6b1964496a2220628ab8dc02d
Tim Fiez <tim@fiezdevelopmentgroup.com>
2016-11-28 21:16:53 -0800

    added heat map

---
+++ public_src/services/querydata.service.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by timfiez on 11/28/16.
+ */
+import { Injectable } from '@angular/core';
+//import {QUERYDATA} from 'mock-query-data';
+
+@Injectable()
+export class QueryDataService {
+    getData(): string { return "test"}
+}
+
+
